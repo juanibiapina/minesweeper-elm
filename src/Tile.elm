f53675a67838a6945adda1b594d93c452f194195
Juan Ibiapina <juanibiapina@gmail.com>
2015-10-29 22:45:08 -0200

    feature: Add content to tiles

--- src/Tile.elm
+++ src/Tile.elm
@@ -7,16 +7,18 @@ import Html.Attributes exposing (style)
 import Mouse
 
 type State = Opened | Closed
+type Content = Mine | Value Int
 
 type alias Model =
   { state: State
+  , content: Content
   }
 
 type Action = Open
 
 init: (Model, Effects Action)
 init =
-  ({ state = Closed } , Effects.none)
+  ({ state = Closed, content = Mine } , Effects.none)
 
 update: Action -> Model -> (Model, Effects Action)
 update action model =
@@ -26,12 +28,23 @@ update action model =
 view: Signal.Address Action -> Model -> Html.Html
 view address model =
   case model.state of
-    Opened -> Html.button [openedStyle] []
+    Opened ->
+      case model.content of
+        Mine -> Html.button [mineStyle] []
+        Value v -> Html.button [openedStyle] [Html.text (toString v)]
     Closed -> Html.button [onClick address Open, closedStyle] []
 
 inputs: List (Signal Action)
 inputs =
   []
+
+mineStyle: Html.Attribute
+mineStyle =
+  style
+    [ ("width", "100px")
+    , ("height", "100px")
+    , ("background-color", "red")
+    ]
 
 openedStyle: Html.Attribute
 openedStyle =
